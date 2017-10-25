6e675780ac2cdeb888be7470d24f7fac24f0b54f
dnzyx <dndszyx@qq.com>
2017-10-25 15:14:39 +0800

    change sth

--- app/src/main/java/com/learning/lenovo/grpc_android/MainActivity.java
+++ app/src/main/java/com/learning/lenovo/grpc_android/MainActivity.java
@@ -21,22 +21,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button b=(Button)findViewById(R.id.ok);
+        final Button b=(Button)findViewById(R.id.ok);
         final EditText name=(EditText)findViewById(R.id.name);
         final EditText pwd=(EditText)findViewById(R.id.pwd);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    MainActivity.client(name.getText().toString(),pwd.getText().toString());
+                    MainActivity.startclient(name.getText().toString(),pwd.getText().toString(),b);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         });
     }
-    public static void client(String name,String pwd) throws InterruptedException {
-        ManagedChannel channel = OkHttpChannelBuilder.forAddress("192.168.49.10", 8080).usePlaintext(true).build();
+    public static void startclient(String name,String pwd,Button b) throws InterruptedException {
+        String ip="10.1.88.120";
+        ManagedChannel channel = OkHttpChannelBuilder.forAddress(ip, 8080).usePlaintext(true).build();
         //同步调用: SendMailServiceGrpc.newBlockingStub(channel)
         //异步调用的话，就是：SendMailServiceGrpc.newFutureStub(channel))
         SendMailServiceGrpc.SendMailServiceBlockingStub stub = SendMailServiceGrpc.newBlockingStub(channel);
@@ -44,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         //设置请求参数
         SendMailRequest param = SendMailRequest.newBuilder().setName(name).setPwd(pwd).build();
         SendMailResponse resp = stub.sendMail(param);
-        System.out.println(resp.getMsg() + "\t" + resp.getCode());
+        b.setText(resp.getMsg());
 
         //close
         channel.shutdown().awaitTermination(1, TimeUnit.SECONDS);
